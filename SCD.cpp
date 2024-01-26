b2c4c77bb837161859497617e38c9344008a80b6
Muhammad Ibrahim Zarar <157349270+MIbrahim2203@users.noreply.github.com>
2024-01-26 22:23:34 +0500

    SCD.cpp

--- SCD.cpp
+++ SCD.cpp
@@ -1,31 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Function to add two numbers
-double add(double a, double b) {
-    return a + b;
-}
 
-// Function to subtract two numbers
-double subtract(double a, double b) {
-    return a - b;
-}
-
-// Function to multiply two numbers
-double multiply(double a, double b) {
-    return a * b;
-}
-
-// Function to divide two numbers
-double divide(double a, double b) {
-    // Check for division by zero
-    if (b == 0) {
-        std::cerr << "Error: Division by zero is not allowed." << std::endl;
-        return 0; // Return 0 in case of division by zero
-    }
-
-    return a / b;
-}
 
 int main() {
     double num1, num2;
